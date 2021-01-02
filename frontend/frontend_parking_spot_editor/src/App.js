6603b3b11b0d4df52888f34e620df7a747bae247
Olesiak Adam (STUD) <01123951@pw.edu.pl>
2021-01-02 16:27:36 +0100

    Initial implemnetation of editing parking spots feature. With URL filtering. Uses mock data.

---
+++ frontend/frontend_parking_spot_editor/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './ParkingSpotEditor'
+import ParkingSpotEditor from './ParkingSpotEditor';
+
+function App() {
+  return (
+    <ParkingSpotEditor/>
+  );
+}
+
+export default App;
